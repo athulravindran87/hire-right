1fe75521281519e65919c0de83a3cb0f01171f87
Athul Ravindran <prasanna29387@gmail.com>
2020-06-24 20:28:31 -0400

    LocalFileService to save files to local filesystem

---
+++ core-lib/src/main/java/com/hright/file/LocalFileService.java
@@ -0,0 +1,33 @@
+package com.hright.file;
+
+import org.apache.commons.io.FileUtils;
+import org.eclipse.collections.impl.factory.Lists;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@ConditionalOnProperty(name = "dm.aws.enabled", havingValue = "false", matchIfMissing = false)
+public class LocalFileService implements FileService {
+
+    @Override
+    public File retrieveFile(String url) {
+        return FileUtils.getFile(url);
+    }
+
+    @Override
+    public String save(File file) {
+        return null;
+    }
+
+    @Override
+    public String saveAll(List<File> files) {
+        Lists.adapt(files)
+                .reject(Objects::isNull)
+                .forEach(this::save);
+        return "";
+    }
+}
---
+++ core-lib/src/test/java/com/hright/file/LocalFileServiceTest.java
@@ -0,0 +1,51 @@
+package com.hright.file;
+
+import com.hright.test.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.collections.impl.factory.Lists;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNull;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LocalFileServiceTest extends BaseTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private File tempFolder;
+
+    private LocalFileService testObj;
+
+    @Before
+    public void setUp() throws Exception {
+        this.testObj = new LocalFileService();
+        tempFolder = temporaryFolder.newFolder();
+    }
+
+    @Test
+    public void retrieveFile() throws Exception {
+        FileUtils.copyFileToDirectory(ResourceUtils.getFile("classpath:testdata/Archive.zip"), new File(tempFolder.getAbsolutePath()));
+        assertThat(this.testObj.retrieveFile(tempFolder.getAbsolutePath() + "/Archive.zip").getName(), equalTo("Archive.zip"));
+    }
+
+    @Test
+    public void save() {
+        assertNull(this.testObj.save(new File("")));
+    }
+
+    @Test
+    public void saveAll() {
+        assertThat(this.testObj.saveAll(Lists.mutable.empty()), equalTo(""));
+    }
+}
