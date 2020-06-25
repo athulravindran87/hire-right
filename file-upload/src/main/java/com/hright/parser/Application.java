4100f98d96c4bac889e4c45ec2304e23509d90e0
Athul Ravindran <prasanna29387@gmail.com>
2020-06-25 17:15:22 -0400

    added support for kafka consumer and producer

--- file-upload/src/main/java/com/hright/parser/Application.java
+++ file-upload/src/main/java/com/hright/parser/Application.java
@@ -2,14 +2,10 @@ package com.hright.parser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.hright")
+@SpringBootApplication(scanBasePackages = "com.hright")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
