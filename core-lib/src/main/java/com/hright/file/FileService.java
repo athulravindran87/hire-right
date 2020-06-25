b2e7836a11656e9d748bbdbb479ce15515386ffc
Athul Ravindran <prasanna29387@gmail.com>
2020-06-24 20:28:05 -0400

    interface for file services

---
+++ core-lib/src/main/java/com/hright/file/FileService.java
@@ -0,0 +1,16 @@
+package com.hright.file;
+
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.List;
+
+@Service
+public interface FileService {
+
+    File retrieveFile(String url);
+
+    String save(File file);
+
+    String saveAll(List<File> files);
+}
