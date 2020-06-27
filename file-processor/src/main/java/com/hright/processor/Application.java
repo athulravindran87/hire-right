1ed1f42fc259a31148d367ec6c1705b00d77c78f
Athul Ravindran <prasanna29387@gmail.com>
2020-06-27 12:37:52 -0400

    enabled kafka

--- file-processor/src/main/java/com/hright/processor/Application.java
+++ file-processor/src/main/java/com/hright/processor/Application.java
@@ -3,9 +3,11 @@ package com.hright.processor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.hright")
+@EnableKafka
 public class Application {
 
     public static void main(String[] args) {
