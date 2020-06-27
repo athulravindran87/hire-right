ffff8c515c98f99030dbe0d3768834f9d7a8ac82
Athul Ravindran <prasanna29387@gmail.com>
2020-06-27 16:43:21 -0400

    fully functional kafka consumer

--- file-processor/src/main/java/com/hright/processor/kafka/KafkaConsumer.java
+++ file-processor/src/main/java/com/hright/processor/kafka/KafkaConsumer.java
@@ -3,8 +3,10 @@ package com.hright.processor.kafka;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.hright.model.Resume;
+import com.hright.processor.parser.service.ProcessorService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -15,11 +17,15 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(name = "spring.kafka.consumer.enabled", havingValue = "true", matchIfMissing = true)
 public class KafkaConsumer {
 
+    @Autowired
+    private ProcessorService processorService;
+
     @KafkaListener(id = "${spring.kafka.consumer.group-id}", topics = "${kafka.topic.file-processor}", containerFactory = "kafkaListenerContainerFactory")
     public void consumeMessage(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
         log.info("Recieved Message from partition: {} key: {}, message: {}", record.partition(), record.key(), record.value());
         try {
             Resume resume = new Gson().fromJson(record.value(), Resume.class);
+            processorService.process(resume);
         } catch (JsonSyntaxException e) {
             log.error("Error cannot convert to Resume Object");
         }
--- file-processor/src/test/java/com/hright/processor/kafka/KafkaConsumerTest.java
+++ file-processor/src/test/java/com/hright/processor/kafka/KafkaConsumerTest.java
@@ -1,30 +1,47 @@
 package com.hright.processor.kafka;
 
+import com.hright.model.Resume;
+import com.hright.processor.parser.service.ProcessorService;
 import com.hright.test.BaseTest;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.kafka.support.Acknowledgment;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.verify;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class KafkaConsumerTest extends BaseTest {
 
-    private KafkaConsumer testObj;
+    @Captor
+    private ArgumentCaptor<Resume> argumentCaptor;
 
-    @Before
-    public void setUp() throws Exception {
-        this.testObj = new KafkaConsumer();
-    }
+    @Mock
+    private ProcessorService processorService;
+
+    @InjectMocks
+    private KafkaConsumer testObj;
 
     @Test
     public void consumeMessage() {
         Acknowledgment acknowledgment = Mockito.mock(Acknowledgment.class);
-        ConsumerRecord<String, String> record = new ConsumerRecord("topic-1", 0, 1L, "key1", "value1");
+        ConsumerRecord<String, String> record = new ConsumerRecord("topic-1", 0, 1L, "key1", "{\"id\":\"key2\",\"body\":\"somebody\",\"resumeUrl\":\"http://someurl\"}");
         this.testObj.consumeMessage(record, acknowledgment);
 
+        verify(this.processorService).process(argumentCaptor.capture());
+        Resume captorValue = argumentCaptor.getValue();
+
+        assertThat(captorValue.getId(), equalTo("key2"));
+        assertThat(captorValue.getBody(), equalTo("somebody"));
+        assertThat(captorValue.getResumeUrl(), equalTo("http://someurl"));
         verify(acknowledgment).acknowledge();
     }
 }
