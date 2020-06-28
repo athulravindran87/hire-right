2e084b721528bffd9dc557655ab93c7fe8881af5
Athul Ravindran <prasanna29387@gmail.com>
2020-06-27 23:38:31 -0400

    added support to handler to send message downstream on elastic topic

--- file-processor/src/main/java/com/hright/processor/disruptor/handlers/SendDownStreamHandler.java
+++ file-processor/src/main/java/com/hright/processor/disruptor/handlers/SendDownStreamHandler.java
@@ -1,15 +1,28 @@
 package com.hright.processor.disruptor.handlers;
 
+import com.hright.MessageProducer;
+import com.hright.model.Resume;
 import com.hright.processor.disruptor.bean.ResumeEvent;
 import com.lmax.disruptor.WorkHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class SendDownStreamHandler implements WorkHandler<ResumeEvent> {
+
+    @Value(value = "${kafka.topic.elastic-search}")
+    private String elasticSearchTopic;
+
+    @Autowired
+    private MessageProducer producer;
+
     @Override
     public void onEvent(ResumeEvent resumeEvent) throws Exception {
-        log.info("sending downstream:, {}", resumeEvent.getResume());
+        Resume resume = resumeEvent.getResume();
+        log.info("sending downstream:, {}", resume);
+        this.producer.sendMessage(elasticSearchTopic, resume.getId(), resume.toJson());
     }
 }
--- file-processor/src/test/java/com/hright/processor/disruptor/handlers/SendDownStreamHandlerTest.java
+++ file-processor/src/test/java/com/hright/processor/disruptor/handlers/SendDownStreamHandlerTest.java
@@ -1,15 +1,48 @@
 package com.hright.processor.disruptor.handlers;
 
+import com.hright.MessageProducer;
+import com.hright.model.Resume;
+import com.hright.processor.disruptor.bean.ResumeEvent;
+import com.hright.test.BaseTest;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-public class SendDownStreamHandlerTest {
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SendDownStreamHandlerTest extends BaseTest {
+
+    @Mock
+    private MessageProducer messageProducer;
+
+    @InjectMocks
+    private SendDownStreamHandler testObj;
+
 
     @Before
     public void setUp() throws Exception {
+        ReflectionTestUtils.setField(this.testObj, "elasticSearchTopic", "topic2");
     }
 
     @Test
-    public void onEvent() {
+    public void onEvent() throws Exception {
+        ResumeEvent event = this.createEvent();
+        this.testObj.onEvent(event);
+
+        verify(this.messageProducer).sendMessage(eq("topic2"), anyString(), anyString());
+    }
+
+    private ResumeEvent createEvent() {
+        ResumeEvent resumeEvent = new ResumeEvent();
+        resumeEvent.setSequenceId(123L);
+        resumeEvent.setResume(Resume.builder().id("123").localFileSystemPath("localFile").build());
+        return resumeEvent;
     }
 }
