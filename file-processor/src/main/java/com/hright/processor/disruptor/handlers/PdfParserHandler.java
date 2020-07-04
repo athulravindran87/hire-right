a6363e3e56fd16719bf310cf8f79adf2c53a206b
Athul Ravindran <prasanna29387@gmail.com>
2020-07-03 20:37:04 -0400

    add loggers

--- file-processor/src/main/java/com/hright/processor/disruptor/handlers/PdfParserHandler.java
+++ file-processor/src/main/java/com/hright/processor/disruptor/handlers/PdfParserHandler.java
@@ -4,9 +4,11 @@ import com.hright.model.Resume;
 import com.hright.processor.disruptor.bean.ResumeEvent;
 import com.hright.processor.parser.service.ParserService;
 import com.lmax.disruptor.WorkHandler;
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 import java.util.Objects;
 
@@ -18,8 +20,8 @@ public class PdfParserHandler implements WorkHandler<ResumeEvent> {
     private ParserService parserService;
 
     @Override
+    @Timed(value = "pdf_parser", description = "pdf parser", longTask = true)
     public void onEvent(ResumeEvent resumeEvent) throws Exception {
-
         try {
             if (Objects.isNull(resumeEvent)) {
                 log.error("Event is null, cannot proceed");
@@ -31,8 +33,15 @@ public class PdfParserHandler implements WorkHandler<ResumeEvent> {
                 log.error("File System url is empty for Resume id: {}, cannot proceed", resume.getId());
                 return;
             }
+            log.info("Performing OCR for resume: {}", resume.getId());
+            StopWatch watch = new StopWatch();
+            watch.start();
 
             this.parserService.parse(resume);
+
+            watch.stop();
+            log.info("Time taken to process resume: {} is {}", resume.getId(), watch.getTotalTimeSeconds());
+
         } catch (Exception e) {
             log.error("Unable to parse PDF document for id: {}", resumeEvent.getResume().getId());
         }
--- file-processor/src/main/java/com/hright/processor/parser/service/ParserService.java
+++ file-processor/src/main/java/com/hright/processor/parser/service/ParserService.java
@@ -3,11 +3,9 @@ package com.hright.processor.parser.service;
 import com.hright.FileService;
 import com.hright.model.Resume;
 import com.hright.processor.parser.tess4j.Tess4jParser;
-import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StopWatch;
 
 import java.io.File;
 
@@ -24,14 +22,8 @@ public class ParserService {
         this.fileService = fileService;
     }
 
-    @Timed
     public void parse(Resume resume) {
-        log.info("Performing OCR for resume: {}", resume.getId());
-        StopWatch watch = new StopWatch();
-        watch.start();
         String body = this.parser.parse(this.retrieveFile(resume.getLocalFileSystemPath()));
-        watch.stop();
-        log.info("Time taken to process resume: {} is {}", resume.getId(), watch.getTotalTimeSeconds());
         resume.setBody(body);
     }
 
