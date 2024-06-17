cf10645244bc91985ee4b1b72797c110617c914a
athulravindran87 <prasanna29387@gmail.com>
2024-06-17 21:58:40 +1000

    added implementation for aws s3 support

--- core-lib/src/main/java/com/hright/remote/AwsFileService.java
+++ core-lib/src/main/java/com/hright/remote/AwsFileService.java
@@ -1,27 +1,49 @@
 package com.hright.remote;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.hright.FileService;
 import com.hright.model.Resume;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.collections.impl.factory.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 
+@Slf4j
 @Service
 @ConditionalOnProperty(name = "dm.aws.enabled", havingValue = "true", matchIfMissing = true)
 public class AwsFileService implements FileService {
 
+    @Autowired
+    private AmazonS3Client amazonS3;
+
+    @Autowired
+    private AwsConfig awsConfig;
+
     @Override
     public File retrieveFile(String url) {
-        return null;
+        return new File(url);
     }
 
     @Override
     public void save(Resume resume) {
+        File file = new File(resume.getLocalFileSystemPath());
+        log.info("Writing file: {} to s3 bucket: {}", file.getName(), this.awsConfig.getBucketName());
+        this.amazonS3.putObject(new PutObjectRequest(this.awsConfig.getBucketName(), file.getName(), file));
+        URL s3Url = this.amazonS3.getUrl(this.awsConfig.getBucketName(), file.getName());
+        log.info("S3 url is " + s3Url.toExternalForm());
+        resume.setResumeUrl(s3Url.toExternalForm());
     }
+
 
     @Override
     public void saveAll(List<Resume> resumes) {
+        Lists.adapt(resumes).reject(Objects::isNull).forEach(this::save);
     }
 }
--- core-lib/src/test/java/com/hright/remote/AwsFileServiceTest.java
+++ core-lib/src/test/java/com/hright/remote/AwsFileServiceTest.java
@@ -1,24 +1,68 @@
 package com.hright.remote;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.hright.model.Resume;
 import com.hright.test.BaseTest;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.factory.Lists;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.File;
+import java.net.URL;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AwsFileServiceTest extends BaseTest {
+
+    @Mock
+    private AmazonS3Client s3Client;
+
+    @Mock
+    private AwsConfig awsConfig;
+
+    @InjectMocks
+    private AwsFileService tesObj;
 
     @Before
     public void setUp() throws Exception {
+        when(this.awsConfig.getBucketName()).thenReturn("resume-bucket");
+        when(this.s3Client.putObject(any())).thenReturn(new PutObjectResult());
+        when(this.s3Client.getUrl("resume-bucket", "file1")).thenReturn(new URL("http://s3/dummy"));
+        when(this.s3Client.getUrl("resume-bucket", "file2")).thenReturn(new URL("http://s3/dummy2"));
     }
 
     @Test
     public void retrieveFile() {
+        assertThat(this.tesObj.retrieveFile("some"), instanceOf(File.class));
     }
 
     @Test
     public void save() {
+        Resume resume = Resume.builder().localFileSystemPath("/somewhere/file1").build();
+        this.tesObj.save(resume);
+        assertThat(resume.getResumeUrl(), equalTo("http://s3/dummy"));
+        verify(this.s3Client).putObject(any());
+        verify(this.s3Client).getUrl(any(), anyString());
     }
 
     @Test
     public void saveAll() {
+        MutableList<Resume> resumes = Lists.mutable.of(Resume.builder().localFileSystemPath("/somewhere/file1").build(),
+                Resume.builder().localFileSystemPath("/somewhere/file2").build());
+
+        this.tesObj.saveAll(resumes);
+        verify(this.s3Client, times(2)).putObject(any());
+        verify(this.s3Client, times(2)).getUrl(any(), anyString());
     }
 }
