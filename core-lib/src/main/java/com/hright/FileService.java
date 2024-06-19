3c7e461787ac77ab9ea206a6c24476998e739600
athulravindran87 <prasanna29387@gmail.com>
2024-06-19 10:44:57 +1000

    upgrade to spring boot latest and java 17

--- core-lib/src/main/java/com/hright/FileService.java
+++ core-lib/src/main/java/com/hright/FileService.java
@@ -1,12 +1,10 @@
 package com.hright;
 
 import com.hright.model.Resume;
-import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.List;
 
-@Service
 public interface FileService {
 
     File retrieveFile(String url);
--- file-upload/src/test/java/com/hright/parser/ApplicationIT.java
+++ file-upload/src/test/java/com/hright/parser/ApplicationIT.java
@@ -5,7 +5,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
